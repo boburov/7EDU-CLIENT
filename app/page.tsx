a67af796b54ba9182bd7c8d3739139d61c64483d
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-06-11 02:14:14 +0500

    finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 "use client"
+import { useEffect } from "react";
 import Home from "./components/Home";
 
 export default function page() {
- 
+  useEffect(() => {
+    const tg = (window as any).Telegram;
+    if (typeof window !== 'undefined' && tg?.WebApp) {
+      tg.WebApp.ready();
+      tg.WebApp.expand();
+    }
+  }, []);
+
   return (
     <div>
       <Home />
