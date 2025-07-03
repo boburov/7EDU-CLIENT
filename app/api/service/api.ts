29e36e181d7dd091364859c017e11abd04955f35
Boburov Shukurullo <boburovdev@gmail.com>
2025-07-03 19:27:46 +0500

    finished

--- app/api/service/api.ts
+++ app/api/service/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import apiEndpoins from '../api.endpoin';
 
 const api = axios.create({
-    baseURL: `http://localhost:3000/`
+    baseURL: `https://sevenedu.store/`
     //  || 'https://sevenedu.store/',
 });
 
