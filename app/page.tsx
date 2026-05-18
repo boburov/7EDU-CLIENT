c1bf636add548e4daab53f4dc864432e93ef0619
boburov <boburovdev@gmail.com>
2026-05-18 20:55:22 +0500

    feat: add video iframe to the page component

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
       }}
     >
       <iframe
-        src="https://player.vimeo.com/video/1189292623?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1"
+        src="https://player.vimeo.com/video/1174505046?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1"
         frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
