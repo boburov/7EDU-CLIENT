964fe3f18dfd8bf62320854b05ae78ca79070fa3
boburov <boburovdev@gmail.com>
2026-05-18 22:17:32 +0500

    fix: update base URL for API to correct endpoint

--- app/api/service/api.ts
+++ app/api/service/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import apiEndpoins from '../api.endpoin';
 
 const api = axios.create({
-    baseURL: 'https://api.sevenedu.store/',
+    baseURL: 'https://api.sevenedu.org',
 })
 
 api.interceptors.request.use((config) => {
