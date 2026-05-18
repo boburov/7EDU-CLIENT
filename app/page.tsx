a9be368cc184dc09f40a30b57ce93f69dedc9678
boburov <boburovdev@gmail.com>
2026-05-18 21:13:09 +0500

    refactor: remove unused video iframe from page component

--- app/page.tsx
+++ app/page.tsx
@@ -10,27 +10,6 @@ export default function Page() {
 
   return (
     <div>
-       <div
-      style={{
-        padding: "56.25% 0 0 0",
-        position: "relative",
-      }}
-    >
-      <iframe
-        src="https://player.vimeo.com/video/1174505046?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1"
-        frameBorder="0"
-        allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-        }}
-        title="ru55"
-      />
-    </div>
       <HomePage />
     </div>
   );
